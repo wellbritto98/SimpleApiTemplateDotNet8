8e2a55a8474b232ed412dd1542ab950e4594b870
José Wellington <josewellington@infowsoftware.com.br>
2024-02-07 22:45:52 -0300

    First Commit

---
+++ SimpleApiTemplate/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleApiTemplate.Data;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+}
